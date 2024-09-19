dc37594a32b88cc52343c6a42e2a2afcd1dd2fb1
soung90 <sounglikane@gmail.com>
2024-09-19 11:29:24 +0900

    코드 수정 및 주석 정리, Level 4 완료

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,78 +1,85 @@
 import java.util.*;
 
 public class BaseballGame {
-    private String randomNumber;
+    //Field 선언
+    private final String randomNumber;  // random 숫자의 field 선언
+    private final int difficultyLevel; // 자리수(난이도)를 저장하는 field 선언
 
-    // 생성자가 random 숫자를 생성한다
-    public BaseballGame() {
-        this.randomNumber = generatedRandNum();
+    //생성자
+    public BaseballGame(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;   //난이도에 따른 자리수 설정
+        this.randomNumber = generatedRandNum(); // 생성자가 random 숫자를 생성한다
     }
 
     //Game의 진행 Method
     public int play() {
         Scanner scanner = new Scanner(System.in);
-        int gameTries = 0; // Game 시도횟수
+        int gameTries = 0; // Game 시도횟수 초기화
 
         //무한 loop 시작
         while (true) {
-            // Player에게 입력값을 받음
-            System.out.print("\nPlayer 숫자 입력 : ");
-            String playerInput = scanner.nextLine();
 
-            //올바른 입력값을 받았는지 검증
-            if (!validateInput(playerInput)) {
-                System.out.println("올바르지 않은 값입니다.");
-                continue;
-            }
+            try {
+                // Player에게 입력값을 받음
+                System.out.print("\nPlayer 숫자 입력: ");
+                String playerInput = scanner.next();
 
-            // Game 시도횟수 증가
-            gameTries++;
+                //올바른 입력값을 받았는지 검증
+                if (!validateInput(playerInput)) {
+                    System.out.println("올바르지 않은 입력값입니다. 1~9 사이에 서로 다른 " + difficultyLevel + "자리 숫자를 입력하세요.");
+                    continue;
+                }
 
-            //Strike와 Ball의 개수 계산 및 정답여부 확인
-            int strikes = countStrike(playerInput);
-            int balls = countBall(playerInput);
+                // Game 시도횟수 증가
+                gameTries++;
 
-            if (strikes == 3) {
-                System.out.println(strikes + " Strikes~");
-                break;
-            } else if (strikes == 0 && balls == 0) {
-                System.out.println("Out");
-            } else {
-                System.out.println(strikes + " Strike(s), " + balls + " Ball(s).");
+                //Strike와 Ball의 개수 계산 및 정답여부 확인
+                int strikes = countStrike(playerInput);
+                int balls = countBall(playerInput);
+
+                if (strikes == difficultyLevel) {
+                    System.out.println("정답입니다.");
+                    break;
+                } else if (strikes == 0 && balls == 0) {
+                    System.out.println("Out");
+                } else {
+                    System.out.println(strikes + " Strike(s), " + balls + " Ball(s).");
+                }
+            } catch (Exception e) {
+                System.out.println("오류 발생:" + e.getMessage());
             }
         }
-
-        System.out.println("정답입니다!!!");
-
-//        System.out.println("\n정답은: " + randomNumber); //정답 출력
         return gameTries;
     }
 
-    // Computer가 1~9 사이에 서로 다른 3자리 random 숫자를 생성하는 Method
+    // Computer가 1~9 사이에 서로 다른 random 숫자를 생성하는 Method.
+    //Random 숫자의 자리수는 Player가 입력한 값의 자리수와 동일해야 합니다.
     private String generatedRandNum() {
         List<Integer> digitsNumbers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 10; i++) {
             digitsNumbers.add(i);
         }
 
         //random 숫자 shuffle
         Collections.shuffle(digitsNumbers);
 
-        //생성된 숫자를 문자열로 변환하고 반환
+        //생성된 숫자를 문자열로 변환하고 문자열을 반환합니다.
         StringBuilder randomNumber = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < difficultyLevel; i++) {
             randomNumber.append(digitsNumbers.get(i));
         }
         return randomNumber.toString();
     }
 
-    //Player가 1~9 사이에 서로 다른 3자리 수를 입력한지 확인하는 Method
+    //Player가 1~9 사이에 서로 다른 수를 입력한지 확인하는 Method
+    //Player가 입력한 값의 자리수는 선택한 난이도와 동일해야 합니다.
     protected boolean validateInput(String input) {
 
-        //Player의 입력이 3자리 수인지 확인
-        if (input.length() != 3) return false;
-
-        //'toCharArray' Method을 통해 Player가 입력한 숫자를 Set으로 변환합니다.
+        //Player의 입력란 자리수 확인
+        if (input.length() != difficultyLevel) {
+            return false;
+        }
+        //'toCharArray' Method을 통해 Player가 입력한 숫자를 char type Set Collection으로 변환합니다.
         //그리고 입력 값에 문자, '0' 또는 중복이 있는지 확인
         Set<Character> numSet = new HashSet<>();
         for (char character : input.toCharArray()) {
@@ -86,7 +93,7 @@ public class BaseballGame {
     //Strike의 수를 세는 Method
     private int countStrike(String input) {
         int strikes = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < difficultyLevel; i++) {
             if (input.charAt(i) == randomNumber.charAt(i)) {
                 strikes++;
             }
@@ -97,7 +104,7 @@ public class BaseballGame {
     //Ball의 수를 세는 Method
     private int countBall(String input) {
         int balls = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < difficultyLevel; i++) {
             if (input.charAt(i) != randomNumber.charAt(i) && randomNumber.indexOf(input.charAt(i)) >= 0) {
                 balls++;
             }
--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,34 @@ import java.util.*;
 
 public class Main {
 
-    private static final List <Integer> gameAttempts = new ArrayList<>(); // Game 기록 저장 List의 생성
+    private static final List<Integer> gameAttempts = new ArrayList<>(); // Game 기록 저장 List의 생성
+    private static int difficultyLevel = 3; // 기본 자리수(난이도)는 3자리로 합니다.
 
     public static void main(String[] args) {
-//        System.out.println("< Game을 시작합니다 !!!>");
-//        System.out.println("Game Over !!! 시도 횟수: " + gamePlayed);
 
-        Scanner inputChoice = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         while (true) {
             System.out.println("\n환영합니다! 원하시는 번호를 입력해주세요");
-            System.out.println("\n1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
-            int choice = inputChoice.nextInt(); // 선택 입력
+            System.out.println("0. 자리수 설정 1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기");
+            int userChoice = input.nextInt(); // 선택 입력
 
-            switch (choice) {
+            switch (userChoice) {
+                case 0:
+                    setNumberCount();  //자리수 정
+                    break;
+
                 case 1:
-                    startGame();
+                    startGame(); // Game 시작
                     break;
 
                 case 2:
-                    gameRecordsDisplay();
+                    gameRecordsDisplay(); // Game 기록 보기
                     break;
 
                 case 3:
                     System.out.println("< 숫자 야구 게임을 종료합니다 >");
-                    return;
+                    return; // Game 종료
 
                 default:
                     System.out.println("올바른 숫자를 입력해주세요!");
@@ -36,22 +39,48 @@ public class Main {
 
     }
 
+    //자리수를 설정하는 Method (0번을 누른시)
+    private static void setNumberCount() {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n설정하고자 하는 자리수를 입력하세요.(3, 4, 5):");
+
+            try {
+                int difficultyChoice = input.nextInt(); //Player로부터 자리수(난이도)의 입력을 받음
+
+                if (difficultyChoice == 3 || difficultyChoice == 4 || difficultyChoice == 5) {
+                    difficultyLevel = difficultyChoice;
+                    System.out.println(difficultyLevel + "자리수 난이도로 설정되었습니다.");
+                    System.out.println("\n< 게임을 시작합니다 >");
+                    break; // 올바른 값 입력시 loop가 종료됩니다.
+                } else {
+                    System.out.println("3, 4, 5 중 하나를 선택해 주세요");
+                }
+            }
+            // 사용자가 숫자가 아닌 값을 입력했을 때 발생하는 예외 처리
+            catch (NumberFormatException e) {
+                System.out.println("입력을 잘 못했습니다. 숫자를 입력해 주세요.");
+            }
+        }
+        startGame(); //자리수 설정 후 Game 시작됩니다.
+    }
+
     // Game을 시작하는 Method (1번을 누른시)
-    private static void startGame () {
-        BaseballGame game = new BaseballGame();
+    private static void startGame() {
+        BaseballGame game = new BaseballGame(difficultyLevel); //BaseballGame Class의 instance화
         int gamePlayed = game.play();
-        gameAttempts.add(gamePlayed);
+        gameAttempts.add(gamePlayed); //시도 횟수 기록 저장
     }
 
     // Game의 기록을 보여주는 Method (2번을 누른시)
     private static void gameRecordsDisplay() {
         if (gameAttempts.isEmpty()) {
             System.out.println("게임의 기록이 없습니다.");
-        }
-        else {
-            System.out.println("< 게임 기록 보기 >");
+        } else {
+            System.out.println("\n< 게임 기록 보기 >");
             for (int i = 0; i < gameAttempts.size(); i++) {
-                System.out.println((i+1) + "번째 게임 : 시도 횟수 - " + gameAttempts.get(i));
+                System.out.println((i + 1) + "번째 게임 : 시도 횟수 - " + gameAttempts.get(i));
             }
         }
     }
